f3afcbcadec421888ff7baf85bef6024ffa64f13
DrHolera <drholera@gmail.com>
2018-03-16 23:31:51 +0200

    Bot works ok local server

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -2,31 +2,36 @@
 
 namespace MGBot\Config;
 
-// Singleton config.
+/**
+ * Class Config.
+ *
+ * Contains configuration for mattermost and gitlab.
+ */
 class Config {
 
-	// We don't need instances here.
-	private function __construct() {}
+  // Update it with your own credentials.
+  public static function getMattermostConfig() {
+    return [
+      'server' => 'http://mattermost_server',
+      'username' => 'MGBot', // username display on chat
+      'icon_url' => 'https://example.xyz/img/MGBot.png', // icon display on chat
+      'token'    => [
+        'channel_token', // Channels token
+      ],
+    ];
+  }
 
-    // Update it with your own credentials.
-	public static function getMattermostConfig() {
-		return [
-			'username' => 'MGBot', // username display on chat
-		    'icon_url' => 'https://example.xyz/img/MGBot.png', // icon display on chat
-		    'token' => [
-			    'token_channels1',
-		        'token_channels2', // Channels token
-	        ]
-        ];
-    }
+  // Update it with your own credentials.
+  public static function getGitlabConfig() {
+    return [
+      'gitlab_url'  => 'https://your.gitlab.com',
+      'api_version' => 'v4',
+      'private_token' => 'USER_TOKEN',
+    ];
+  }
 
-    // Update it with your own credentials.
-    public static function getGitlabConfig() {
-    	return [
-    		'gitlab_url' => 'https://your.gitlab.com',
-    		'api_version' => 'v4',
-    		'project_id' => 525,
-    	];
-    }
+  // We don't need instances here.
+  private function __construct() {
+  }
 
 }
--- src/Mattermost/MergeChecker.php
+++ src/Mattermost/MergeChecker.php
@@ -1,22 +1,63 @@
-<?php 
+<?php
 
 namespace MGBot\Mattermost;
 
+use GuzzleHttp\Client;
 use MGBot\Config\Config;
 
 class MergeChecker {
 
-	const OPENED_MERGE_REQUESTS = '/merge_requests?state=opened';
+  const OPENED_MERGE_REQUESTS = '/merge_requests?state=opened';
 
-	public function getMergeRequestsCount() {
-    return $this->getGitlabFullUrl();
-	}
+  public function runCheck() {
+    $mergeRequestsCount = $this->getMergeRequestsCount();
+    if ( $mergeRequestsCount >= 5 ) {
+      $this->displayMessage( $mergeRequestsCount );
+    }
+  }
 
+  public function getMergeRequestsCount() {
+    $url      = $this->getGitlabFullUrl();
+    $guzzle   = new Client();
+    $response = $guzzle->request( 'GET', $url )->getBody()->getContents();
 
-	protected function getGitlabFullUrl() {
-		$config = Config::getGitlabConfig();
+    if ( ! empty( $response ) ) {
+      return count( json_decode( $response ) );
+    }
 
-		return $config['gitlab_url'] . '/api/' . $config['api_version'] . '/projects/' . $config['project_id'] . static::OPENED_MERGE_REQUESTS;
-	}
+    die('Empty GitLab response');
+  }
+
+  public function displayMessage( $mergeRequestsCount ) {
+    $matterMostConfig = Config::getMattermostConfig();
+    $guzzle           = new Client();
+
+    // Possibility to run it on several channels.
+    foreach($matterMostConfig['token'] as $webHook) {
+      $guzzle->request( 'POST', $matterMostConfig['server'] . '/hooks/' . $webHook, [
+        'headers' => [
+          'Content-Type' => 'application/json',
+        ],
+        'json'    => [
+          "username" => $matterMostConfig['username'],
+          "icon_url" => $matterMostConfig['icon_url'],
+          "text"     => "Guys! We have too many MRs. $mergeRequestsCount for now! Check it when you'll have time.",
+        ],
+      ] );
+    }
+  }
+
+  protected function getGitlabFullUrl() {
+    $params = getopt('', ['project_id:']);
+    if (empty($params['project_id'])) {
+      die('Project ID should be specified as --project_id param');
+    }
+    $config = Config::getGitlabConfig();
+
+    return $config['gitlab_url'] . '/api/' .
+           $config['api_version'] . '/projects/' .
+           $params['project_id'] . static::OPENED_MERGE_REQUESTS .
+           '&private_token=' . $config['private_token'];
+  }
 
 }
